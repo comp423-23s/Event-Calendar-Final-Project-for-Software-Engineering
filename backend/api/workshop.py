d482b702185d08348ed35122a99751818aa97a27
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 <haella@live.unc.edu>
2023-04-03 13:47:53 -0400

    added docs to workshop api

--- backend/api/workshop.py
+++ backend/api/workshop.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter, Depends
-#from ..services import UserService
 from ..models import Workshop
 from ..services.workshop import WorkshopService
-#from .authentication import registered_user
 
 api = APIRouter(prefix="/api/workshop")
 
-
+#returns list of workshop models
 @api.get("", response_model=list[Workshop], tags=['Workshops'])
 def list_workshops(workshop_svc: WorkshopService = Depends()) -> list[Workshop]:
     return workshop_svc.list()
