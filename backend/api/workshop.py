5a2f2024945c65783323aae361fb2187471d3146
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 <haella@live.unc.edu>
2023-04-03 14:20:16 -0400

    explained args

--- backend/api/workshop.py
+++ backend/api/workshop.py
@@ -4,7 +4,7 @@ from ..services.workshop import WorkshopService
 
 api = APIRouter(prefix="/api/workshop")
 
-#returns list of workshop models
+#returns list of workshop models. It takes in a workshop_service as a dependency
 @api.get("", response_model=list[Workshop], tags=['Workshops'])
 def list_workshops(workshop_svc: WorkshopService = Depends()) -> list[Workshop]:
     return workshop_svc.list()
