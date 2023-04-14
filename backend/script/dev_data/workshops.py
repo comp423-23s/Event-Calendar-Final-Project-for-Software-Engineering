2e57538aa0b7ae4176f01933c83aec706cae5973
Emma Lozevski <emlozevs@email.unc.edu>
2023-04-14 13:46:56 -0400

    Prep for pull request

--- backend/script/dev_data/workshops.py
+++ backend/script/dev_data/workshops.py
@@ -7,13 +7,13 @@ from datetime import datetime
 from . import users
 
 
-workshop1 = Workshop(id=1, title="Workshop1", description="this is a sample description for workshop1", location="Sitterson Hall", date=datetime(2023, 4, 5, 12, 0), host_id=users.merritt_manager.id)
+workshop1 = Workshop(id=1, title="Workshop 1", description="This is a sample description for Workshop 1.", location="Sitterson Hall", date=datetime(2023, 4, 5, 12, 0), host_id=users.merritt_manager.id)
 
-workshop2 = Workshop(id=2, title="Workshop2", description="this is a sample description for workshop2", location="Phillips Hall", date=datetime(2023, 4, 10, 3, 0), host_id=users.sol_student.id)
+workshop2 = Workshop(id=2, title="Workshop 2", description="This is a sample description for Workshop 2.", location="Phillips Hall", date=datetime(2023, 4, 10, 3, 0), host_id=users.sol_student.id)
 
-workshop3 = Workshop(id=3, title="Workshop3", description="this is a sample description for workshop3", location="Dey Hall", date=datetime(2023, 4, 12, 4, 30), host_id=users.sol_student.id)
+workshop3 = Workshop(id=3, title="Workshop 3", description="This is a sample description for Workshop 3.", location="Dey Hall", date=datetime(2023, 4, 12, 4, 30), host_id=users.sol_student.id)
 
-workshop4 = Workshop(id=4, title="Workshop4", description="this is a sample description for workshop4", location="Peabody Hall", date=datetime(2023, 4, 20, 1, 20), host_id=users.merritt_manager.id)
+workshop4 = Workshop(id=4, title="Workshop 4", description="This is a sample description for Workshop 4.", location="Peabody Hall", date=datetime(2023, 4, 20, 1, 20), host_id=users.merritt_manager.id)
 
 models = [
     workshop1, 
