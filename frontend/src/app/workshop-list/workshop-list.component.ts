1d74266b9ebd02da58f28143e95818db598a3b63
Shannon Little <97571121+Littlesk@users.noreply.github.com>
2023-04-16 14:48:55 -0400

    Sprint1 issue33 (#50)
    * created delete service with working delete button
    
    * added comments

--- frontend/src/app/workshop-list/workshop-list.component.ts
+++ frontend/src/app/workshop-list/workshop-list.component.ts
@@ -3,6 +3,7 @@ import { isAuthenticated } from 'src/app/gate/gate.guard';
 import { ActivatedRoute, Route } from '@angular/router';
 import { Workshop, WorkshopListService } from '../workshop-list.service';
 import { Observable } from 'rxjs';
+import { WorkshopDeleteService } from '../workshop-delete.service'
 
 @Component({
   selector: 'app-workshop-list',
@@ -14,7 +15,7 @@ export class WorkshopListComponent {
   public workshops$: Observable<Workshop[]>;
   workshopService: WorkshopListService
 
-  constructor(workshopService: WorkshopListService) {
+  constructor(workshopService: WorkshopListService, private workshopDeleteService: WorkshopDeleteService) {
     this.workshops$ = workshopService.getWorkshops();
     this.workshopService = workshopService;
   }
@@ -43,4 +44,42 @@ export class WorkshopListComponent {
     this.workshops$ = this.workshopService.getWorkshops();
     return this.workshopService.getWorkshops();
   }
+  
+  deleteWorkshop(id: number){
+     /*
+    Calls the workshop delete service and then the deleteWorkshop function. On success it calls onDelSuccess() on error it calls onDelError(). 
+
+    Args:
+      id: number.
+    
+    Returns:
+      None.
+
+    Raises:
+      Error.
+
+    */
+    this.workshopDeleteService.deleteWorkshop(id)
+    .subscribe({
+      next: (msg)=> this.onDelSuccess(msg),
+      error: (err) => this.onDelError(err)
+    })
+  }
+
+  //Gives a success message and updates workshop list, since one has been deleted.
+  onDelSuccess(msg: Workshop){
+      window.alert("Workshop has been deleted.")
+      this.workshops$ = this.workshopService.getWorkshops();
+  }
+
+  //Passes on error message to user.
+  onDelError(err: Error){
+    if(err.message){
+      window.alert(err.message)
+    }
+    else{
+      window.alert("Unknown error: " + JSON.stringify(err))
+    }
+  }
+
 }
