1fcda77d801b82f6bbaa2b42b45cd2ffc5ca442f
Shannon Little <littlesk@ad.unc.edu>
2023-04-15 23:00:51 -0400

    Revert "updated to give create workshop api the new workshop structure and connected html to component (#48)"
    This reverts commit c865463d591a86f05b36016d0ceb64c473604549.

--- frontend/src/app/workshop-create/workshop-create.component.ts
+++ frontend/src/app/workshop-create/workshop-create.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { isAuthenticated } from 'src/app/gate/gate.guard';
 import { ActivatedRoute, Route } from '@angular/router';
 import {FormBuilder} from '@angular/forms';
-import { Workshop} from '../workshop-list.service';
+import { Workshop } from '../workshop-list.service';
 import { WorkshopCreateService } from '../workshop-create.service';
 import { Profile } from '../profile/profile.service';
 import { shareReplay } from 'rxjs';
@@ -19,23 +19,11 @@ interface User{
 }
 
 
-interface WorkshopReturned {
-  id: number,
-  title: String;
-  description: String | null;
-  location: String | null;
-  date: Date | null;
-  host_id: number | null
-  host: User | null
-}
-
-
 @Component({
   selector: 'app-workshop-create',
   templateUrl: './workshop-create.component.html',
   styleUrls: ['./workshop-create.component.css']
 })
-
 
 
 export class WorkshopCreateComponent {
@@ -89,16 +77,16 @@ export class WorkshopCreateComponent {
     let location = form.location ?? "";
 
     this.workshopCreateService
-      .createWorkshop(title, description, location, date, this.profileID)
+      .createWorkshop(title, description, location, date, this.profileID, this.user)
       .subscribe({
-        next: (WorkshopReturned) => this.onSuccess(WorkshopReturned),
+        next: (workshop) => this.onSuccess(workshop),
         error: (err) => this.onError(err)
       })
   }
 
   //Handles success message that occures when trying to create a workshop during the onSumbit function.
   //Nice to have: Make it so  it is not window alerts but intergated into the website HTML.
-  private onSuccess(crtedWorkshop: WorkshopReturned): void{
+  private onSuccess(crtedWorkshop: Workshop): void{
     window.alert('Workshop has been created!');
     this.form.reset(); 
   }
